dcc41a7a62ac3d36c6409e1be51c24f94bd47f83
Christophe VG <xtof@pumpkin.local>
2011-01-01 20:28:17 +0100

    added focus-based growing/shrinking of images improved general layout

--- js/site.js
+++ js/site.js
@@ -4,6 +4,8 @@ var CircularMenu = Class.extend( {
   init : function init( id ) {
     this.id = id;
     this.offset = 0.0;
+    this.radius = 130;
+
     this.setupMenu();
     this.setupItems();
     this.arrangeElements();
@@ -11,27 +13,28 @@ var CircularMenu = Class.extend( {
   },
   
   setupMenu : function setupMenu() {
-    this.ul = document.getElementById(this.id);
-    this.width = parseInt(this.ul.offsetWidth);
-    this.height = parseInt(this.ul.offsetHeight);
+    this.menu   = document.getElementById(this.id);
+    this.width  = parseInt(this.menu.offsetWidth);
+    this.height = parseInt(this.menu.offsetHeight);
+    this.paddingTop = parseInt(this.menu.offsetTop);
+    this.center = { x: this.width / 2, y: this.height / 2 };
   },
   
   setupItems : function setupItems() {
     this.items = [];
-    $A(this.ul.getElementsByTagName("LI")).iterate( function( item ) {
-      this.items.push( new CircularMenu.Item( item ) );
+    $A(this.menu.getElementsByTagName("IMG")).iterate( function( image ) {
+      var item  = new CircularMenu.Item( image );
+      this.items.push( item );
     }.scope(this) );
   },
   
   arrangeElements : function arrangeElements() {
     var blocks = this.items.length;
-    var radius = 180;
-    var center = { x : 200, y : 200 };
     this.items.iterate( function( item, count ) {
       var deg = ( ( ( Math.PI * 2 ) / blocks ) * count ) + this.offset;
-      var x = Math.cos(deg) * radius;
-      var y = Math.sin(deg) * radius;
-      item.goto( center.x + x, center.y - y );
+      var x = Math.cos(deg) * this.radius;
+      var y = Math.sin(deg) * this.radius;
+      item.moveTo( this.center.x + x, this.paddingTop + this.center.y - y );
     }.scope(this) );
   },
   
@@ -44,18 +47,76 @@ var CircularMenu = Class.extend( {
 
 CircularMenu.Item = Class.extend( {
   init : function init( elem ) {
-    this.li = elem;
-    this.li.style.position = "absolute";
-    this.li.style.float    = "none";
-    this.width = this.li.offsetWidth;
-    this.height = this.li.offsetHeight;
-    this.center = { x : this.width / 2, y : this.height /2 };
+    this.element = elem;
+    this.analyzeElement();
+    
+    this.scale = 0.60;
+    this.setupElement();
+    
+    this.updateElement();
   },
   
+  analyzeElement : function analyzeElement() {
+    this.width = this.element.offsetWidth;
+    this.height = this.element.offsetHeight;
+    this.center = { x : this.width / 2, y : this.height /2 };
+  },
+
+  setupElement : function setupElement() {
+    this.element.style.position = "absolute";
+    this.element.style.float    = "none";
+    ProtoJS.Event.observe( this.element, "mouseover", 
+                           this.handleFocus.scope(this) );
+    ProtoJS.Event.observe( this.element, "mouseout", 
+                           this.handleLostFocus.scope(this) );
+  },
+  
+  updateElement : function updateElement() {
+    var width = this.width * this.scale;
+    var height = this.height * this.scale;
+    this.element.style.width  = width  + "px";
+    this.element.style.height = height + "px";
+  },
+    
+  processModification : function processModification() {
+    if( this.scaleDelta == 0 ) { return; }
+
+    // scaling
+    this.scale += this.scaleDelta;
+    if( this.scale >= 1.00 ) { this.scale = 1.00; this.scaleDelta = 0; }
+    if( this.scale <  0.60 ) { this.scale = 0.60; this.scaleDelta = 0; }
+
+    this.updateElement();
+    
+    this.processModification.scope(this).after(30); 
+  },
+  
+  handleFocus     : function handleFocus()     {  this.grow();  },
+  handleLostFocus : function handleLostFocus() { this.shrink(); },
+  
+  grow : function grow() {
+    this.scaleDelta = 0.10;
+    this.processModification()
+  },
+  
+  shrink : function shrink() {
+    this.scaleDelta = -0.10;
+    this.processModification();
+  },
+
   goto : function goto(left, top) {
-    // TODO: animate movement
-    this.li.style.left = ( this.width + left ) + "px";
-    this.li.style.top  = ( top ) + "px";
+    this.left = left;
+    this.top  = top;
+    var paddingLeft = this.element.parentNode.offsetLeft;
+    var dx = ( this.width * this.scale ) / 2;
+    var dy = ( this.height * this.scale ) / 2;
+    this.element.style.left = ( paddingLeft + this.left - dx ) + "px";
+    this.element.style.top  = ( this.top - dy ) + "px";
+  },
+  
+  moveTo : function moveTo(left, top) {
+    // TODO: animate ;-)
+    this.goto(left, top);
   }
 } );
 

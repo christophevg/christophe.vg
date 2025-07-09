9e0f7b7a0ca822d4751cccf001d063170400c6f3
Christophe VG <contact@christophe.vg>
2025-07-09 10:21:10 +0200

    added "veggie" tag filter

--- assets/js/custom/category_filter.js
+++ assets/js/custom/category_filter.js
@@ -1,10 +1,20 @@
 (function() {
   var pages = $(".grid__item");
   $("#grid__filter").keyup(function(event) {
-    var query = event.target.value.toLowerCase();
+    var query = event.target.value.toLowerCase(), veggie_only = false;
+    if( query.includes("veggie") ) {
+      veggie_only = true;
+      query = query.replace("veggie", "").replace("  ", " ").trim();
+    }
     pages.detach();
-    $(".grid__wrapper").append(query.length < 2 ? pages : pages.filter(function(idx, page){
-      return $(page).find("a").text().toLowerCase().includes(query);
+    $(".grid__wrapper").append(pages.filter(function(idx, page) {
+      var page_matches_query = $(page).find("a").text().toLowerCase().includes(query) || query.length < 2,
+          page_is_veggie     = $(page).find("div.green").text().includes("Veggie");
+      if(veggie_only) {
+        return page_matches_query && page_is_veggie;
+      } else {
+        return page_matches_query;
+      }
     }));
   });
 })();
